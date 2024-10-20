7143c3edaf61dbf4f53e5a32dbb98ff1dfcedf67
manonlespes <manonlespes@hotmail.fr>
2024-10-20 10:21:14 +0200

    style(card): About section - card content added wip

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,44 @@
 export const Card = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4">
-        <div className="card lg:card-side bg-base-100 shadow-xl">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="card bg-base-100 shadow-xl">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-              alt="Album"
+              className="object-cover aspect-[4/3] "
+              src="/images/manon_lespes.webp"
+              alt="Manon Lespes - in a train looking trhough the window."
             />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">New album is released!</h2>
-            <p>Click the button to listen on Spotiwhy app.</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Listen</button>
-            </div>
+          <div className="card-body px-5 md:px-8">
+            <h3 className="card-title">
+              This is me, pretending to look pensively at the landscape
+            </h3>
+            <p>
+              Before the pandemic, I was organising events and then I changed my
+              career direction and jumped into the digital and IT world. I like
+              coding, but also traveling, dancing and reading. But most of all,
+              I am fond of my cat. I know, I sound like a cliché.
+            </p>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl">
+          <figure>
+            <img
+              className="object-cover aspect-[4/3] bg-fixed"
+              src="/images/chatshimiendormie.jpg"
+              alt="Chatshimi, my lovely spleeping cat."
+            />
+          </figure>
+          <div className="card-body px-5 md:px-8">
+            <h2 className="card-title">Chatshimi, the Spleeping Beauty</h2>
+            <p>
+              Chatshimi sometimes acts as my rubber-cat-debugging. When I'm
+              working remotly, she's constantly by my side and even when she's
+              taking a 10-hour power nap, she helps me debugg. Rest assured that
+              the code is in safe and capable pawls and hands.
+            </p>
           </div>
         </div>
       </div>
