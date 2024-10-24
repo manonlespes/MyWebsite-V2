24866069a1bfc2cce54d073d618896dd6219b405
manonlespes <manonlespes@hotmail.fr>
2024-10-24 19:09:24 +0200

    doc(button & link) typescript interface

--- src/components/types/ComponentsTypes.ts
+++ src/components/types/ComponentsTypes.ts
@@ -1,9 +1,20 @@
 export interface CardContentType {
-  key?: number;
   title?: string;
   text?: string;
   img?: {
     alt: string;
     source: string;
   };
+}
+export interface LinkType {
+  label: string;
+  src: string;
+  ariaLabel?: string;
+  isExternalLink: boolean;
+}
+
+export interface ButtonType {
+  label: string;
+  isLink?: boolean;
+  linkTo?: string;
 }
