dc7d5028da2f40a94fec2bf18629b3afe4edf798
manonlespes <manonlespes@hotmail.fr>
2024-10-24 19:10:36 +0200

    refact(button): add typescript + link router

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,26 @@
-export const Button = (props: { label: string }) => {
-  const { label } = props;
+import { Link } from "react-router-dom";
+import { ButtonType } from "./types/ComponentsTypes";
+
+export const Button = (props: ButtonType) => {
+  const { label, isLink, linkTo } = props;
   return (
     <>
-      <button className="btn-sm md:btn-md rounded-lg bg-primary-500 text-neutral-content relative overflow-hidden group z-10 hover:text-neutral-content duration-1000">
-        <span className="absolute bg-primary-600 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
-        <span className="absolute bg-primary-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
-        {label}
-      </button>
+      {isLink && linkTo ? (
+        <Link
+          to={linkTo}
+          className="btn-sm md:btn-md rounded-lg bg-primary-500 text-neutral-content relative overflow-hidden group z-10 hover:text-neutral-content duration-1000"
+        >
+          <span className="absolute bg-primary-600 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
+          <span className="absolute bg-primary-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
+          {label}
+        </Link>
+      ) : (
+        <button className="btn-sm md:btn-md rounded-lg bg-primary-500 text-neutral-content relative overflow-hidden group z-10 hover:text-neutral-content duration-1000">
+          <span className="absolute bg-primary-600 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
+          <span className="absolute bg-primary-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
+          {label}
+        </button>
+      )}
     </>
   );
 };
