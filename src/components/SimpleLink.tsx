26a7c8ffe783c961ea1a14ca7ffc8c85ca1b32e8
manonlespes <manonlespes@hotmail.fr>
2024-11-02 12:45:24 +0100

    style(link): fix style hover link when too long

--- src/components/SimpleLink.tsx
+++ src/components/SimpleLink.tsx
@@ -15,14 +15,16 @@ export const SimpleLink = (props: LinkType) => {
       {isExternalLink ? (
         <a
           key={id}
-          className={`inline-flex items-baseline group text-base link link-animation text-inherit ${className}`}
+          className={`group text-base link link-animation text-inherit ${className}`}
           href={src}
           target="_blank"
           rel="noreferrer noopener"
           aria-label={ariaLabel + "(opens in a new tab)"}
         >
-          {label}
-          <NewWindowIcon className="h-4 w-4 " />
+          <span>
+            {label}
+            <NewWindowIcon className="h-4 w-4" />
+          </span>
         </a>
       ) : (
         <a
