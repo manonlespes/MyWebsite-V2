0feecc7a357a92652ff963b4c2eb26d6e0b6cc1b
manonlespes <manonlespes@hotmail.fr>
2025-02-18 13:05:30 +0100

    (navbar): fix a11y

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,12 +39,14 @@ export const Navbar: React.FC = () => {
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <span className="sr-only">Menu</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
+                aria-hidden
               >
                 <path
                   strokeLinecap="round"
