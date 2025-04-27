e0dfa84f510758ff589da2f2e27b14aa664f723b
manonlespes <manonlespes@hotmail.fr>
2025-04-27 15:45:15 +0200

    (button): fix a11y focus on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,30 +12,30 @@ export const Button = ({
       {isLink && linkTo ? (
         <Link
           to={linkTo}
-          className={`${className} w-fit p-3 text-sm md:text-base rounded-lg bg-primary-500 text-neutral-content relative !overflow-hidden group z-10 hover:text-neutral-content duration-1000`}
+          className={`${className} w-fit p-3 text-sm md:text-base rounded-lg bg-primary-500 text-neutral-content relative !overflow-hidden group z-10 hover:text-neutral-content focus:text-neutral-content duration-1000`}
         >
           <span
             aria-hidden
-            className="absolute bg-primary-600 w-40 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"
+            className="absolute bg-primary-600 w-40 h-36 rounded-full group-hover:scale-100 group-focus:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 group-focus:duration-500 duration-700 origin-center transform transition-all"
           ></span>
           <span
             aria-hidden
-            className="absolute bg-primary-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"
+            className="absolute bg-primary-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 group-focus:scale-100 scale-0 -z-10 group-hover:duration-700 group-focus:duration-700 duration-500 origin-center transform transition-all"
           ></span>
           {label}
         </Link>
       ) : (
         <button
           type="submit"
-          className={`${className} btn-md text-sm md:text-base rounded-lg bg-primary-500 text-neutral-content relative !overflow-hidden group z-10 hover:text-neutral-content duration-1000`}
+          className={`${className} btn-md text-sm md:text-base rounded-lg bg-primary-500 text-neutral-content relative !overflow-hidden group z-10 hover:text-neutral-content focus:text-neutral-content duration-1000`}
         >
           <span
             aria-hidden
-            className="absolute bg-primary-600 w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"
+            className="absolute bg-primary-600 w-36 h-36 rounded-full group-hover:scale-100 group-focus:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 group-focus:duration-500 duration-700 origin-center transform transition-all"
           ></span>
           <span
             aria-hidden
-            className="absolute bg-primary-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"
+            className="absolute bg-primary-800 w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 group-focus:scale-100 scale-0 -z-10 group-hover:duration-700 group-focus:duration-700 duration-500 origin-center transform transition-all"
           ></span>
           {label}
         </button>
