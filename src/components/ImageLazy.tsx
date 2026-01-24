1333ee60cfebd7d48b5a5d842a573c0b6d3e7b70
manonlespes <manonlespes@hotmail.fr>
2026-01-24 12:57:45 +0100

    (image): fix size img in about page

--- src/components/ImageLazy.tsx
+++ src/components/ImageLazy.tsx
@@ -2,11 +2,17 @@ import { InView } from "react-intersection-observer";
 import Image from "./Image";
 import { ImageType } from "./types/ComponentsTypes";
 
-const ImageLazy = ({ image }: { image: ImageType }) => {
+const ImageLazy = ({
+  image,
+  className = "",
+}: {
+  image: ImageType;
+  className?: string;
+}) => {
   return (
     <InView triggerOnce>
       {({ ref, inView }) => (
-        <div ref={ref}>
+        <div ref={ref} className={className}>
           <Image image={image} inView={inView} />
         </div>
       )}
